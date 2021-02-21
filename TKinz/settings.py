ddb380a114ad3b86e8c7469f612aa697a6f5b9c8
Tomasz Kordek <toma.kordek@gmail.com>
2021-02-21 09:27:43 +0000

    Remove old stuff

--- TKinz/settings.py
+++ TKinz/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'sfdb2un^s#yzkb^_!*_e8u5du&l@nc&0z0b!izr9r(%a$_-x$_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tkordek.pythonanywhere.com']
 
 
 # Application definition
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pl-PL'
 
-TIME_ZONE = 'europe/Warsaw'
+#TIME_ZONE = 'europe/Warsaw'
 
 USE_I18N = True
 
@@ -119,5 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
